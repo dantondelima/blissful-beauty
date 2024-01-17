c4be886f11000638288d63036ffafb6422e94dfa
Danton <dantondelima.contato@gmail.com>
2024-01-17 20:56:00 -0300

    feat: :sparkles: add staff members registration and repository interaction

---
+++ src/Domain/Repository/StaffMemberRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Domain\Repository;
+
+use App\Domain\Model\StaffMember;
+use App\Domain\Model\User;
+use PDO;
+
+interface StaffMemberRepositoryInterface
+{
+    public function __construct(PDO $pdo);
+    public function allMembers(): array;
+    public function insert(StaffMember $staffMember): bool;
+    public function fireStaffMember(StaffMember $staffMember): bool;
+}
